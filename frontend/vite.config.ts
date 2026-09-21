5c90fd07588251c4ac782cf5fa134f10b2f7f89e
jackra1n <45038833+jackra1n@users.noreply.github.com>
2026-09-21 23:36:43 +0200

    fix: order UnoCSS before SvelteKit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,8 +5,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
-		sveltekit(),
 		UnoCSS(),
+		sveltekit(),
 		paraglideVitePlugin({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide',
